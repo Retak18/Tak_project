c6ecb572f5283825735bde6e99fdf658c7da4db2
retak18 <tarek_lamarti@live.fr>
2025-09-23 10:23:45 +0200

    Add  Analytics in the application

--- tak_project/src/App.js
+++ tak_project/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Gallery from './components/Gallery/Gallery';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Analytics />
       <Gallery />
     </div>
   );
