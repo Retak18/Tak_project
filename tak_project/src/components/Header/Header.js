2a46d5f94f68baf600f19128d3a1e47d0a7c9803
retak18 <tarek_lamarti@live.fr>
2025-09-24 11:42:02 +0200

    Update header

--- tak_project/src/components/Header/Header.js
+++ tak_project/src/components/Header/Header.js
@@ -9,7 +9,15 @@ const Header = () => {
         <span className='subtitle'>[ Portfolio Gallery ]</span>
       </h1>
       <p>Bienvenue dans ma galerie de portfolio. Ici, vous pourrez voir mes projets, le développement et les détails d'organisation.</p>
-      <p className="click">Cliquez sur les images pour voir les détails</p>
+      <div className="social">
+      <a  href="mailto:tarek_lamarti@live.fr">
+        <img className="mail" src="\mail_l.png" alt="Mail" />
+      </a>
+      <a className="link" href="https://www.linkedin.com/in/tarek-lamarti-06b041b9/" target="_blank" >
+        <img src="\linkedin-icon.svg" alt="LinkedIn" />
+      </a>
+    </div>
+      <p className="click">N'hésitez pas à cliquer sur les images ci-dessous pour voir les détails</p>
     </header>
   );
 };
